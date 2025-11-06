bbe820e2bf623b287738d7b2e589f565b8520a0f
Susanta Ghosh <susanta938269@gmail.com>
2025-11-06 20:23:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+function validateNumber() {
+    let number = document.querySelector('input[name="number"]').value;
+    if (!/^\d{10}$/.test(number)) {
+        alert("Please enter a valid 10-digit mobile number.");
+        return false; // stop form submission
+    }
+    return true;
+}
+
